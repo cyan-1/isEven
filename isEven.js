88a618d1a7f42ebbc24b34d0cbc6f57205172d94
MakeshiftJuliette <71993095+Cinnamonsroll@users.noreply.github.com>
2023-07-31 00:26:00 -0400

    Handling isNaN exceptions

--- isEven.js
+++ isEven.js
@@ -10,7 +10,7 @@ async function isEven(number) {
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-            {"role": "system", "content": "You will be provided with a number you will reply with either true or false depnding if the number is even or not, in all lower case"}, 
+            {"role": "system", "content": "You will be provided with a number you will reply with either true or false depnding if the number is even or not, in all lower case, however if it isn't a number you will say 'not a number'"}, 
             {role: "user", content: `${number}`}
         ],
     });
